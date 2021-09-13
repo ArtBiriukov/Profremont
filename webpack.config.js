3a7df785269452c8bbdbf79215549dc1f89645bd
Artem <carbon0093@gmail.com>
2021-09-13 11:41:15 +0300

    config: настройка webpack, и подключение JS

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'bundel'),
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, ''),
+    },
+    open: true,
+    compress: true,
+    hot: true,
+    port: 9000,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}

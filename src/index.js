1ac8376e96ee3c281a034955996efaeac1e51982
Artem <carbon0093@gmail.com>
2021-09-14 13:13:07 +0300

    step: начало разработки слайдера

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import modals from "./mobules/modals";
 import scrollTop from "./mobules/scrollTop";
 import timer from "./mobules/timer";
+import sliders from "./mobules/sliders";
 
 modals();
 scrollTop();
 timer('23 September  2021');
+sliders();

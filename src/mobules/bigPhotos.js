fb5ba7dc6292155a70788fb5532104646725e5b3
Artem <carbon0093@gmail.com>
2021-09-16 12:49:02 +0300

    fix: modal photo

--- src/mobules/bigPhotos.js
+++ src/mobules/bigPhotos.js
@@ -5,18 +5,18 @@ const bigPhoto = () => {
     contentPhoto = photoModal.querySelector('.modal-content');
 
   const callModal = () => {
+
     overlay.classList.toggle('active-menu');
     scrollBtn.classList.toggle('active-zindex');
     photoModal.classList.toggle('active-menu');
   };
 
   document.addEventListener('click', event => {
-    event.preventDefault();
-
     const target = event.target;
 
     //callModal
     if (target.closest('.document-overlay')) {
+      event.preventDefault();
       const bigPhoto = target.closest('.sertificate-document').href;
 
       contentPhoto.src = `${bigPhoto}`;
